e0337be02ef4bdc91bbdc95b79d6597af84134fa
Mcfly150321 <20arturomaldonado@umes.edu.gt>
2026-08-25 19:31:36 -0600

    agregando carpetas para meter servicios externos logicas

--- app/config.py
+++ app/config.py
@@ -1,15 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    # Declaras tus variables de entorno y sus tipos
     DATABASE_URL2: str
 
-    # Pydantic busca automáticamente el archivo .env en la raíz del proyecto
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
 
-# Instancia global accesible en toda la app
 settings = Settings()
--- app/utils/codesgen.py
+++ app/utils/codesgen.py
@@ -1,1 +1,4 @@
-#hola
+import secrets
+def generar_codigo_verificacion() -> str:
+    codigo_completo = "".join(str(secrets.randbelow(10)) for _ in range(8))
+    return codigo_completo
