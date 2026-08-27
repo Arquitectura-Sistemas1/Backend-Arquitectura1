4ad3ca91521766c63c27e8112ce1f25fda67009a
Mcfly150321 <20arturomaldonado@umes.edu.gt>
2026-08-26 19:21:11 -0600

    agregando cors middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 # Importas el router que acabamos de armar
 from app.api.router import router as usuarios_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite cualquier origen para pruebas
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite GET, POST, OPTIONS, etc.
+    allow_headers=["*"],  # Permite Content-Type, ngrok-skip-browser-warning, etc.
+)
 #Registramoss el router en la aplicación
 app.include_router(usuarios_router)
 
